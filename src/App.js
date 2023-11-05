3318d889d8ade19441067ae8006a1060ba2f7efe
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-05 14:14:00 +0300

    Understanding useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+
+  let [count, setCount] = useState(0);
+  let [text, setText] = useState("");
+
+
+  useEffect(() =>{
+    document.title = text;
+  },
+  [text]);
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    setTimeout(() => setCount(0), 5000)
+  }, [])
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+  <div>
+    <button onClick={() => setCount((count) = count + 1)}>
+      I have been clicked {count} times
+    </button>
+
+    <input
+      type="text"
+      placeholder="Type away..."
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+
+  </div>
+    
+    )
 }
 
 export default App;
